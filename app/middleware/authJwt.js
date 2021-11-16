aa72834c864cbe94d7cc0040c124fbba82ac4259
Muhammad Yusuf Auliya <aulusuf@gmail.com>
2021-11-16 17:00:09 +0700

    update

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -1,43 +1,41 @@
-const jwt = require('jsonwebtoken')
-const config = require('../config/auth.config')
-const db = require('../models')
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config");
+const db = require("../models");
 const User = db.user;
 
 verifyToken = (req, res, next) => {
-    let token = req.headers['x-access-token'];
+  let token = req.headers["x-access-token"];
 
-    if (!token) {
-        return res.status(403).send({
-            message: "Token not provided"
-        })
+  if (!token) {
+    return res.status(403).send({
+      message: "Token not provided",
+    });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized",
+      });
     }
+    req.userId = decoded.id;
+    next();
+  });
+};
 
-    jwt. verify(token, config.secre, (err, decoded)=>{
-        if (err) {
-            return res.status(401).send({
-                message: "Unauthorized",
-            })
+isManajemen = (req, res, next) => {
+  User.findByPk(req.userId).then((user) => {
+    user.getRoles().then((roles) => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].id === user.roleId) {
+          next();
+          return;
         }
-        req.userId = decoded.id
-        next()
-    })
-}
-
-isManajemen = (req,res,next)=> {
-    User.findByPk(req.userId)
-    .then((user)=> {
-        user.getRoles()
-        .then((roles)=> {
-            for( let i = 0; i < roles.length; i++ ) {
-                if(roles[i].id === user.roleId) {
-                    next()
-                    return
-                }
-            }
-            res.status(404).send({
-                message: "Not Found"
-            })
-            return
-        })
-    })
-}
+      }
+      res.status(404).send({
+        message: "Not Found",
+      });
+      return;
+    });
+  });
+};
